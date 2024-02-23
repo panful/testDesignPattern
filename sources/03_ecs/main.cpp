5c6b7ebff0d5fd959794e378a266dd70447d1104
worker-Windows <yangpan0822@qq.com>
2024-02-23 10:08:10 +0800

    ECS架构

--- sources/03_ecs/main.cpp
+++ sources/03_ecs/main.cpp
@@ -1,93 +1,158 @@
 #include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
-// 定义组件
-struct PositionComponent {
+/// @brief 实体
+struct Entity
+{
+    int id;
+
+    bool operator==(Entity const& e) const noexcept
+    {
+        return e.id == this->id;
+    }
+};
+
+namespace std {
+template <>
+struct hash<Entity>
+{
+    using argument_type = Entity;
+    using result_type   = size_t;
+
+    result_type operator()(argument_type const& e) const
+    {
+        return e.id;
+    }
+};
+
+} // namespace std
+
+/// @brief 位置组件
+struct PositionComponent
+{
     float x, y;
 };
 
-struct RenderComponent {
-    std::string sprite;
+/// @brief 速度组件
+struct VelocityComponent
+{
+    float x, y;
 };
 
-// 定义实体
-struct Entity {
-    int id;
-};
-
-// 定义系统
-class System {
+/// @brief 系统基类
+class System
+{
 public:
     virtual void Update() = 0;
 };
 
-class RenderSystem : public System {
+class MoveSystem : public System
+{
 public:
-    RenderSystem(std::unordered_map<int, PositionComponent>& positions, std::unordered_map<int, RenderComponent>& renderables)
-        : positions_(positions), renderables_(renderables) {}
+    MoveSystem(Entity e) : entity_(e)
+    {
+    }
 
-    void Update() override {
-        for (const auto& pair : renderables_) {
-            int entityId = pair.first;
-            if (positions_.find(entityId) != positions_.end()) {
-                const PositionComponent& position = positions_[entityId];
-                const RenderComponent& renderable = renderables_[entityId];
-                std::cout << "Render " << renderable.sprite << " at (" << position.x << ", " << position.y << ")\n";
-            }
-        }
+    void Update() override
+    {
+        // TODO
+        // auto dt      = 1.f;
+        // auto pos     = ECS.GetPosition();
+        // auto vel     = ECS.GetVelocity();
+        // auto nextPos = pos + vel * dt;
+        // ECS.SetPosition(entity_, nextPos);
+
+        static int test { 0 };
+        std::cout << test++ << '\n';
     }
 
 private:
-    std::unordered_map<int, PositionComponent>& positions_;
-    std::unordered_map<int, RenderComponent>& renderables_;
+    Entity entity_;
 };
 
-// ECS管理器
-class ECS {
+/// @brief ECS管理器 EntityManager
+class ECS
+{
 public:
-    Entity CreateEntity() {
-        Entity entity{nextEntityId_++};
+    Entity CreateEntity()
+    {
+        Entity entity { nextEntityId_++ };
         entities_.push_back(entity);
         return entity;
     }
 
-    void AddComponent(Entity entity, PositionComponent position) {
-        positions_[entity.id] = position;
+    void AddComponent(Entity entity, PositionComponent position)
+    {
+        positions_[entity] = position;
     }
 
-    void AddComponent(Entity entity, RenderComponent renderable) {
-        renderables_[entity.id] = renderable;
+    void AddComponent(Entity entity, VelocityComponent velocity)
+    {
+        velocities_[entity] = velocity;
     }
 
-    void Update() {
-        for (auto& system : systems_) {
+    void AddSystem(System* system)
+    {
+        systems_.emplace_back(system);
+    };
+
+    PositionComponent GetPosition(Entity e) const
+    {
+        return positions_.at(e);
+    }
+
+    VelocityComponent GetVelocity(Entity e) const
+    {
+        return velocities_.at(e);
+    }
+
+    void SetPosition(Entity e, PositionComponent pos)
+    {
+        positions_[e] = pos;
+    }
+
+    void SetVelocity(Entity e, VelocityComponent vel)
+    {
+        velocities_[e] = vel;
+    }
+
+    void Update()
+    {
+        for (auto& system : systems_)
+        {
             system->Update();
         }
     }
 
-public:
+private:
     std::vector<Entity> entities_;
-    std::unordered_map<int, PositionComponent> positions_;
-    std::unordered_map<int, RenderComponent> renderables_;
+    std::unordered_map<Entity, PositionComponent> positions_;
+    std::unordered_map<Entity, VelocityComponent> velocities_;
     std::vector<System*> systems_;
     int nextEntityId_ = 1;
 };
 
-int main() {
+int main()
+{
     ECS ecs;
+
     Entity entity1 = ecs.CreateEntity();
+    ecs.AddComponent(entity1, PositionComponent { 0.0f, 0.0f });
+    ecs.AddComponent(entity1, VelocityComponent { 0.0f, 2.0f });
+
     Entity entity2 = ecs.CreateEntity();
+    ecs.AddComponent(entity2, PositionComponent { 1.0f, 2.0f });
+    ecs.AddComponent(entity2, VelocityComponent { 1.0f, 0.0f });
 
-    ecs.AddComponent(entity1, {10.0f, 20.0f});
-    ecs.AddComponent(entity2, {30.0f, 40.0f});
-    ecs.AddComponent(entity1, {"PlayerSprite"});
-    ecs.AddComponent(entity2, {"EnemySprite"});
+    MoveSystem move1(entity1);
+    MoveSystem move2(entity2);
 
-    RenderSystem renderSystem(ecs.positions_, ecs.renderables_);
-    ecs.systems_.push_back(&renderSystem);
+    ecs.AddSystem(&move1);
+    ecs.AddSystem(&move2);
 
-    while (true) {
+    while (true)
+    {
         ecs.Update();
     }
 
