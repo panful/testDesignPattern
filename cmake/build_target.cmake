f433dcc4c8fdd3c60250b52dc3ef63ae11d4db05
worker-Windows <yangpan0822@qq.com>
2024-02-29 09:46:57 +0800

    调整目录结构

--- cmake/build_target.cmake
+++ cmake/build_target.cmake
@@ -1,17 +1,18 @@
 function(BuildTarget path)
     file(GLOB subPaths ${path}/*)
-
     foreach(subPath ${subPaths})
-        get_filename_component(target_name ${subPath} NAME)
-        file(GLOB_RECURSE sources ${subPath}/*.cpp ${subPath}/*.h)
-        set(target_name "${target_name}")
-
-        # executable
-        add_executable(${target_name} ${sources})
-
-        # install
-        install(TARGETS ${target_name} RUNTIME DESTINATION .)
-
+        string(REGEX MATCH "([^/]+)/([0-9]+)_([^/]+)$" match_result ${subPath})
+        if(match_result)
+            file(GLOB __subPath ${subPath}/*)
+            foreach(subProj ${__subPath})
+                get_filename_component(target_name ${subProj} NAME)
+                file(GLOB_RECURSE target_sources ${subProj}/*.cpp ${subProj}/*.h)
+                if(target_sources)
+                    set(target_name ${CMAKE_MATCH_2}_${target_name})
+                    add_executable(${target_name} ${target_sources})
+                    install(TARGETS ${target_name} RUNTIME DESTINATION .)
+                endif(target_sources)
+            endforeach(subProj ${__subPath})
+        endif(match_result)
     endforeach(subPath ${subPaths})
-
 endfunction(BuildTarget path)
